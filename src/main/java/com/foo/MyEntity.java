6f07d60ec47201a16f1461be05710c37241f992e
wil <wil.huunguyen@gmail.com>
2020-07-11 09:40:43 +0700

    Version is not initialized

--- src/main/java/com/foo/MyEntity.java
+++ src/main/java/com/foo/MyEntity.java
@@ -1,7 +1,44 @@
 package com.foo;
 
 import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import dev.morphia.annotations.Version;
 
 @Entity
 public class MyEntity {
+    @Id
+    private String id;
+    @Version
+    private Long version;
+    private String description;
+
+// FIXME - Uncomment this block and the test will pass
+//    @PrePersist
+//    private void onSave() {
+//        version = version == null ? 1l : version;
+//    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(final Long version) {
+        this.version = version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
 }
--- src/test/java/com/foo/ReproducerTest.java
+++ src/test/java/com/foo/ReproducerTest.java
@@ -1,10 +1,62 @@
 package com.foo;
 
-import dev.morphia.TestBase;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ReproducerTest extends TestBase {
+import java.util.Arrays;
+import java.util.List;
+
+public class ReproducerTest {
     @Test
     public void reproduce() {
+        final MongoClient mongoClient = provideMongoCLient();
+        final Datastore datastore = datastore(mongoClient);
+        this.clearOldData(datastore);
+
+        // Create new entities
+        final List<MyEntity> entities = Arrays.asList(createEntity(), createEntity());
+        final List<MyEntity> saved = datastore.save(entities);
+
+        // Fail here
+        Assert.assertThat(saved.get(0).getVersion(), CoreMatchers.notNullValue());
+
+        // Expected
+        saved.stream().peek(entity -> entity.setDescription("Updatable")).forEach(datastore::save);
+    }
+
+    private MyEntity createEntity() {
+        final MyEntity entity = new MyEntity();
+        entity.setDescription("Created");
+        return entity;
+    }
+
+    private MongoClient provideMongoCLient() {
+        final String connectionUrl = "mongodb://awesome:thenewawesomecompanyontopoftheworld@localhost:27017/awesome";
+        final ConnectionString connString = new ConnectionString(connectionUrl);
+        final MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connString)
+            .retryWrites(true)
+            .build();
+        return MongoClients.create(settings);
+    }
+
+    private Datastore datastore(final MongoClient mongoClient) {
+
+        final Datastore datastore = Morphia.createDatastore(mongoClient, "awesome");
+        datastore.getMapper().mapPackage("com.foo");
+        datastore.ensureIndexes();
+
+        return datastore;
+    }
+
+    private void clearOldData(final Datastore datastore) {
+        datastore.find(MyEntity.class).iterator().forEachRemaining(datastore::delete);
     }
 }
